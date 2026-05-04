a212aacbf54400cdad38cab925ac804e6a7e2cfc
Robert Linsalato <linsalato.2022@gmail.com>
2026-05-04 16:46:07 -0700

    Changed homepage stuff

--- src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
+++ src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
@@ -1,4 +1,3 @@
-// src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
 import { onlyPages } from "@lekoarts/gatsby-theme-jodie/src/utils/resolver-templates"
 
 const modifyGrid = (data) => onlyPages(data)
