9e017cfec2edf09b5deed71902304b1f07d0390b
Robert Linsalato <linsalato.2022@gmail.com>
2026-05-04 16:28:10 -0700

    fixed logo i hope

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const Logo = () => (
-  <svg width="100%" height="100%" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.5">
+  <svg width="100%" height="100%" viewBox="0 0 33 33" version="1.1" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.5">
   <circle cx="7.5" cy="7.5" r="7.2" fill="#e7e7e7" stroke="#000" stroke-width="1"/>
   <path d="M6.7,9.1c-0.3,-2.6 -1.3,-4.9 -2,-6.8c-1.4,-3.4 -4.8,-2.5 -8,-1.3c0.3,2.8 0.8,5.9 1.2,9l-1.4,0l-0.9,-16c0,0 11,2 15,3c0.8,0 1.6,0 2.4,-0.2c9,-1 12,6 4,8c5,-0 4,5 5,8l-0.2,0c-3,0 -6,0 -9,0Z" stroke="#000" stroke-width="0.5"/>
   <path d="M4.2,7l-0.3,-7c0,0 9,-1 11,-2c1,-0 3,-0 4,3c1,3 -2,5 -5,6c-3,1 -9,3 -9,3" fill="#e7e7e7" stroke="#000" stroke-width="0.5"/>
