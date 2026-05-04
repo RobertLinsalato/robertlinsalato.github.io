af66f1a3f45a3840a0be71e543d7c1b838d2e613
Robert Linsalato <linsalato.2022@gmail.com>
2026-05-04 16:34:35 -0700

    cfgvhj

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 
 const Logo = () => (
-  <svg width="100%" height="100%" viewBox="0 0 33 33" version="1.1" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.5">
-  <circle cx="7.5" cy="7.5" r="7.2" fill="#e7e7e7" stroke="#000" stroke-width="1"/>
-  <path d="M6.7,9.1c-0.3,-2.6 -1.3,-4.9 -2,-6.8c-1.4,-3.4 -4.8,-2.5 -8,-1.3c0.3,2.8 0.8,5.9 1.2,9l-1.4,0l-0.9,-16c0,0 11,2 15,3c0.8,0 1.6,0 2.4,-0.2c9,-1 12,6 4,8c5,-0 4,5 5,8l-0.2,0c-3,0 -6,0 -9,0Z" stroke="#000" stroke-width="0.5"/>
-  <path d="M4.2,7l-0.3,-7c0,0 9,-1 11,-2c1,-0 3,-0 4,3c1,3 -2,5 -5,6c-3,1 -9,3 -9,3" fill="#e7e7e7" stroke="#000" stroke-width="0.5"/>
-  <path d="M9.2,4l1,7l5,-2l-0,-2l-4,2l-1,-6l-2,1Z" stroke="#000" stroke-width="0.5"/>
+  <svg width="100%" height="100%" viewBox="0 0 240 240" version="1.1" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.5">
+  <circle cx="120" cy="120" r="114.96" fill="#e7e7e7" stroke="#000" stroke-width="10"/>
+  <path d="M107.28,145.2c-0.047,-1.101 -0.504,-2.038 -0.864,-2.882c-4.164,-9.771 -17.907,-4.356 -40.416,-0.478c0.427,10.027 1.28,30.08 1.92,45.12l-22.56,3.36l-1.44,-111.6c0,0 33.89,-6.669 56.64,-11.04c1.225,-0.235 2.443,-0.245 3.639,-0.331c30.661,-2.206 37.266,45.997 11.961,55.531c16.62,-0.523 12.745,32.994 14.4,49.44l-0.24,0.72c-7.28,1.44 -20.926,4.545 -21.84,4.32c0,0 -0.653,-19.343 -1.2,-32.16Z" stroke="#000" stroke-width="2"/>
+  <path d="M66.24,123.84l-0.48,-29.76c0,0 28.653,-5.492 31.68,-6.24c3.027,-0.748 10.97,-1.086 12.96,9.84c1.924,10.559 -2.936,16.874 -8.16,18.48c-5.224,1.606 -36,7.68 -36,7.68" fill="#e7e7e7" stroke="#000" stroke-width="2"/>
+  <path d="M137.76,60.48l1.44,111.12l76.08,-14.16l-0.24,-19.92l-53.28,10.32l-1.2,-91.68l-22.8,4.32Z" stroke="#000" stroke-width="2"/>
 </svg>
 
 )
